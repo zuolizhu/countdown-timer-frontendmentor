d364cfd16f9d8b50b5a03b52ad76048061b9f4f4
zuolizhu <zuolizhu5960x@gmail.com>
2020-12-13 16:23:44 -0500

    project init with base font setup

---
+++ src/components/FlipClock.js
@@ -0,0 +1,7 @@
+export default function FlipClock() {
+  return (
+    <div className="flip-clock">
+      
+    </div>
+  )
+}
---
+++ src/components/Heading.js
@@ -0,0 +1,7 @@
+export default function Heading() {
+  return (
+    <header className="header">
+      <h1 className="t-heading">We're launching soon</h1>
+    </header>
+  )
+}
